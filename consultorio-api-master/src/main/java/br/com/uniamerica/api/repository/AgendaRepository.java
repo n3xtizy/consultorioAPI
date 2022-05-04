29600bbc8bbb06e43794693a68b558d742210976
Gabriel Martins <90357161+n3xtizy@users.noreply.github.com>
2022-05-04 20:37:12 -0300

    Add files via upload

---
+++ consultorio-api-master/src/main/java/br/com/uniamerica/api/repository/AgendaRepository.java
@@ -0,0 +1,8 @@
+package br.com.uniamerica.api.repository;
+
+import br.com.uniamerica.api.entity.Agenda;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AgendaRepository extends JpaRepository<Agenda, Long> { }
